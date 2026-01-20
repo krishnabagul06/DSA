556825870adc868ef857129aa089d0dcb0e96913
krishnabagul06 <krishnabagul06@gmail.com>
2026-01-20 11:29:50 +0530

    Notes

---
+++ star3.cpp
@@ -0,0 +1,29 @@
+//coloum sementic
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cin>>n;
+    for(i=0;i<=n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            cout<<"  ";
+        }
+        for(j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        for(j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        for(j=1;j<=n-i;j++)
+        {
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
+}
---
+++ star5.cpp
@@ -0,0 +1,29 @@
+//coloum asementic
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cin>>n;
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            cout<<"  ";
+        }
+        for(j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        for(j=1;j<=i-1;j++)
+        {
+            cout<<"* ";
+        }
+        for(j=1;j<=n-i;j++)
+        {
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
+}
