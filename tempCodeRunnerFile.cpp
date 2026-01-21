0ba3996122d030f1a1c4687b5b9753bc9104174f
krishnabagul06 <krishnabagul06@gmail.com>
2026-01-21 14:09:10 +0530

    pattern

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,5 +1,15 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(int j=i;j<=n-i;j++)
         {
-          
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
