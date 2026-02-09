3648303b724edb9390310aa553f85fa44026c5d2
krishnabagul06 <krishnabagul06@gmail.com>
2026-02-09 14:47:17 +0530

    Array

---
+++ min_value.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+#include<climits>
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int ans=INT_MAX;
+    for(int i=0; i<n;i++)
+    {
+        if(ans>arr[i])
+        {
+            ans=arr[i];
+        }
+    
+    }
+    cout<<ans<<endl;
+    return 0;
+}
